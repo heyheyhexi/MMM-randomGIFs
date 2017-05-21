1ad6756a3680fb76f72b44753680ab2989227e26
heyheyhexi <tobger@aol.com>
2017-05-21 13:05:21 +0200

    first test

--- MMM-randomGIFs.js
+++ MMM-randomGIFs.js
@@ -9,17 +9,34 @@
 
 Module.register("MMM-randomGIFs",{
 	defaults: {
-		url: 'http://replygif.net/random'
+//		url: 'http://replygif.net/random'
+		imageSize: 600,
 	},
 	
-//	start: function () {
-//		var source = document.location.href = this.config.url;
-//		
-//	},
+	start: function setup() {
+		noCanvas();
+		var api = "http://api.giphy.com/v1/gifs/random?"
+		var apiKey = "api_key=dc6zaTOxFJmzC"
+		var query = ""
+		var url = api + apiKey + query;
+		loadJSON(url, gotData);	
+	},
+			function gotData(giphy){
+			var creategif = createImg(giphy.data[0].image_url);
+			}
 
 	getDom: function() {
 		var wrapper = document.createElement("div");
-		wrapper.innerHTML = '<iframe src="http://replygif.net/random" style="width:600px;height:600px;"></iframe>';
+			wrapper.style.height = 0.98 * this.config.imageSize - 1 + "px";
+			wrapper.style.overflow = "hidden";
+		
+		var image = document.createElement("img");
+		image.src = createImg
+//		wrapper.innerHTML = '<iframe src="http://replygif.net/random" style="width:600px;height:600px;"></iframe>';
+		image.width = this.config.imageSize.toString();
+		image.height = this.config.imageSize.toString();
+		
+		wrapper.appendChild(image);
 		return wrapper;
 	},	
 };
